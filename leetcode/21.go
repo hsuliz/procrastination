90f9c73a7b587396753375b957c08db5a18f3a32
hsuliz <hlib.suliz@gmail.com>
2025-07-04 15:20:57 +0200

    feat: add list note

--- leetcode/21.go
+++ leetcode/21.go
@@ -1,10 +1,5 @@
 package leetcode
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	head := &ListNode{}
 	current := head
